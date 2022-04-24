a288ce8dad3cc10b0d2394621d726464fe868e2b
cr34t1ve <mrsofua@gmail.com>
2022-04-24 17:29:48 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,130 +26,55 @@ class GradientText extends StatefulWidget {
 }
 
 class _GradientTextState extends State<GradientText> {
-
-  // color of the circle
-  Color color = Colors.greenAccent;
+  int xValue = 0;
+  int yValue = 0;
 
   // event returned from accelerometer stream
-   AccelerometerEvent? event;
+  AccelerometerEvent? event;
 
   // hold a refernce to these, so that they can be disposed
-   Timer? timer;
   StreamSubscription? accel;
 
-  // positions and count
-  double top = 125;
-   double? left;
-  int count = 0;
-
-  // variables for screen size
-   double? width;
-   double? height;
-
-  setColor(AccelerometerEvent event) {
-    // Calculate Left
-    double x = ((event.x * 12) + ((width! - 100) / 2));
-    // Calculate Top
-    double y = event.y * 12 + 125;
-
-    // find the difference from the target position
-    var xDiff = x.abs() - ((width! - 100) / 2);
-    var yDiff = y.abs() - 125;
-
-    // check if the circle is centered, currently allowing a buffer of 3 to make centering easier
-    if (xDiff.abs() < 3 && yDiff.abs() < 3) {
-      // set the color and increment count
-      setState(() {
-        color = Colors.greenAccent;
-        count += 1;
-      });
-    } else {
-      // set the color and restart count
-      setState(() {
-        color = Colors.red;
-        count = 0;
-      });
-    }
+  int rangeMapping(
+      int input, int inputStart, int inputEnd, int outputStart, int outputEnd) {
+    int output = (outputStart +
+            ((outputEnd - outputStart) / (inputEnd - inputStart)) *
+                (input - inputStart))
+        .toInt();
+    return output;
   }
 
-  setPosition(AccelerometerEvent event) {
-    if (event == null) {
-      return;
-    }
-
-    // When x = 0 it should be centered horizontally
-    // The left positin should equal (width - 100) / 2
-    // The greatest absolute value of x is 10, multipling it by 12 allows the left position to move a total of 120 in either direction.
-    setState(() {
-      left = ((event.x * 12) + ((width! - 100) / 2));
-    });
-
-    // When y = 0 it should have a top position matching the target, which we set at 125
-    setState(() {
-      top = event.y * 12 + 125;
-    });
-  }
-
-
-  startTimer() {
+  startEvent() async{
     // if the accelerometer subscription hasn't been created, go ahead and create it
     if (accel == null) {
-      accel = accelerometerEvents.listen((AccelerometerEvent eve) {
+      accel = await accelerometerEvents.listen((AccelerometerEvent eve) {
         setState(() {
           event = eve;
+          xValue = rangeMapping(event!.x.toInt(), -10, 10, 0, 255);
+          yValue = rangeMapping(event!.y.toInt(), -10, 10, 0, 255);
         });
       });
     } else {
       // it has already ben created so just resume it
       accel!.resume();
     }
-
-    // Accelerometer events come faster than we need them so a timer is used to only proccess them every 200 milliseconds
-    if (timer == null || !timer!.isActive) {
-      timer = Timer.periodic(Duration(milliseconds: 200), (_) {
-        // if count has increased greater than 3 call pause timer to handle success
-        // if (count > 3) {
-        //   pauseTimer();
-        // } else {
-        //   // proccess the current event
-        //   setColor(event!);
-        //   setPosition(event!);
-        // }
-      });
-    }
   }
 
-  pauseTimer() {
-    // stop the timer and pause the accelerometer stream
-    timer!.cancel();
-    accel!.pause();
-
-    // set the success color and reset the count
-    setState(() {
-      count = 0;
-      color = Colors.green;
-    });
-  }
-    @override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    startTimer();
+    startEvent();
   }
 
   @override
   void dispose() {
-    timer!.cancel();
     accel!.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-      // get the width and height of the screen
-    width = MediaQuery.of(context).size.width;
-    height = MediaQuery.of(context).size.height;
-
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -159,8 +84,8 @@ class _GradientTextState extends State<GradientText> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                          Text('x: ${(event!.x ?? 0).toStringAsFixed(3)}'),
-          Text('y: ${(event!.y ?? 0).toStringAsFixed(3)}'),
+                Text('x: ${(event!.x).toStringAsFixed(3)}'),
+                Text('y: ${(event!.y).toStringAsFixed(3)}'),
               ],
             ),
           ),
@@ -173,8 +98,8 @@ class _GradientTextState extends State<GradientText> {
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
                     colors: [
-                      Color.fromARGB(255, ((event!.y * 9) + ((width! - 200) / 2)).toInt(), ((event!.x * 9) + ((width! - 50) / 2)).toInt(), 255),
-                      Color.fromARGB(255, ((event!.y * 9) + ((height! - 200) / 2)).toInt(), ((event!.x * 9) + ((width! - 50) / 2)).toInt(), 255),
+                      Color.fromARGB(255, xValue, 239, 255),
+                      Color.fromARGB(255, yValue, 97, 255)
                     ]).createShader(
                   Rect.fromLTWH(0, 0, bounds.width, bounds.height),
                 ),
